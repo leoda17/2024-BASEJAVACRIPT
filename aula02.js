ee2d0bf44815299dea0a51829a439fa97021b27a
lele <140726302+leoda17@users.noreply.github.com>
2024-08-20 11:30:22 -0300

    Add files via upload

---
+++ aula02.js
@@ -0,0 +1,26 @@
+    // var -> Criando uma Variavel 
+
+    // var altura = 5;
+    // var comprimento = 8;
+
+    // var area = altura * comprimento ;
+    // console.log(area);
+
+    // Criando uma variavek ( forma mais atualizada de criar uma variavel )
+
+    let altura = 5;
+    let comprimento = 8;
+    let forma = "retangulo"
+    let area;
+
+    if (forma ==="retangulo"){
+        area + altura * comprimento
+    }else{
+        area + (altura * comprimento) / 2 
+    }
+
+    console.log()
+
+    // const -> Ultilizada para criar "Variaveis que são fixas que não se alteram"
+
+    
