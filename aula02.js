de66b4df9fc299bd8075c8c39b4bf578d388e821
lele <140726302+leoda17@users.noreply.github.com>
2024-08-21 11:41:01 -0300

    Add files via upload

--- aula02.js
+++ aula02.js
@@ -1,26 +1,62 @@
-    // var -> Criando uma Variavel 
+// var -> criando uma variável
 
-    // var altura = 5;
-    // var comprimento = 8;
+//var altura = 5;
+//var comprimento = 8;
 
-    // var area = altura * comprimento ;
-    // console.log(area);
+//area = altura * comprimento
+//console.log(area);
 
-    // Criando uma variavek ( forma mais atualizada de criar uma variavel )
+// let -> Cria uma variável ( forma mais atualizada de criar uma variável)
 
-    let altura = 5;
-    let comprimento = 8;
-    let forma = "retangulo"
-    let area;
+const altura = 5;
+const comprimento = 8;
+const forma = "morango";
+let area;
 
-    if (forma ==="retangulo"){
-        area + altura * comprimento
-    }else{
-        area + (altura * comprimento) / 2 
-    }
+if(forma === "retângulo"){
+    area = altura * comprimento
+}else if(forma === "triângulo"){
+    area = (altura * comprimento) / 2
+} else{
+    console.log("Deveria ser uma forma")
+}
+console.log(area);
 
-    console.log()
+// const -> Utilizada para criar "variáveis" que são fixas, que não se alteram
+// Condições booleanas (Truly ou falsy)
+// 0 = false
+// 1 = true
 
-    // const -> Ultilizada para criar "Variaveis que são fixas que não se alteram"
+const usuarioLogado = true;
+const contaPaga = false;
 
-    
+console.log(0 == false); // True
+console.log(contaPaga == 0); // True
+console.log(contaPaga === 0); // False
+console.log("" == false); // True
+console.log(1 == true); // False
+
+// Nulo e Indefinido
+// Nulo -> vazio ou nada
+
+let vitor;
+let luana = null;
+let numero = 3;
+let texto = "Eu gosto de programação <3"
+
+// Consultando os tipos de variáveis;
+
+console.log(typeof vitor);
+console.log(typeof luana);
+console.log(typeof numero);
+console.log(typeof texto);
+
+// Conversão de variável
+
+const numeroNumber = 390;
+const numeroString = "390"
+
+console.log(numeroNumber == numeroString); // compara apenas o valor
+console.log(numeroNumber === numeroString); // compara também o tipo
+console.log(numeroNumber + numeroString); // concatenação
+console.log(numeroNumber + Number(numeroString)) // Soma de números
---
+++ aula03.js
@@ -0,0 +1,41 @@
+// Alguns erros comuns
+
+//const numero: Primeiro erro nao declarar uma constante
+
+
+const minhaVar="oi" // 2° erro: nao declarar uma variavel e chamar no codigo
+console.log(minhaVar); // exemplo: chamar minhavar com V minusculo
+
+console.error('isso esta errado') // 3° "erro": Voçe pode declarar um erro com o console.error:
+
+// Condicional abreviada if() {}
+
+let idadeMinima = 18;
+let idadeCliente = 16;
+
+if(idadeCliente >= idadeMinima){
+    console.log("cerveja")
+} else{
+    console,log("todinho")
+}
+
+//Valor ternario
+console.log(idadeCliente >= IdadeMinima ? "cerveja" : "agua")
+// modo ternario, utilizado a estrutura ___ >= ___ ? ___ : ___
+// modo ternario, utilizado a estrutura ___ <= ___ ? ___ : ___
+// modo ternario, utilizado a estrutura ___ == ___ ? ___ : ___
+
+//Template string
+
+const name = "Eduardo Reis";
+const idade = 17;
+const cidadeNatal = "sanjuan"
+
+const apresentaçao = "Meu nome e" + Nome + ",minha idade e" + idade + "nasci em " + cidadeNatal;
+
+console.log(apresentaçao)
+
+//template string
+const aoresentaçao = `Meu nome e ${nome}, minha idade e ${idade} e minha cidade natal e ${cidadenatal}`
+console.log(apresentaçao2)
+
