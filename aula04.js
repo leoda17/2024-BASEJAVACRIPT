2ccc404981b76186125859bc6212c99337b17b25
lele <140726302+leoda17@users.noreply.github.com>
2024-08-22 12:14:23 -0300

    Add files via upload

--- aula04.js
+++ aula04.js
@@ -1,24 +1,24 @@
-//,FUNÇOES!!!!
+// FUNÇÕES!!!!
 
 let x = "nada";
 console.log(x)
-x = "oi"
-y = 'Tchau'
+x = "Oi"
+y = "Tchau"
 
-// Declaraçao de dunçao
+// Declaração de função
 
 imprimeTexto(y)
 
-// 1) Declarar a funçao
+// 1) Declara a função
 
 function imprimeTexto(texto) {
-    console. log (texto)
+    console.log(texto)
 }
 
-// 2) Chamar/executar a funçao 
+// 2) Chamar/executar a função
 
 imprimeTexto(x)
-imprimeTexto(" EU sou aluno")
+imprimeTexto("Eu sou aluno")
 
 function soma(){
     return 2 + 2
@@ -26,24 +26,50 @@ function soma(){
 
 console.log(soma())
 imprimeTexto(soma())
-imprimeTexto(someDeNumero(4, 7))
-imprimeTexto(somaDeNumero(77, 33))
+imprimeTexto(somaDeNumeros(4, 7))
+imprimeTexto(somaDeNumeros(24, 11))
 
-function somaDeNumeris(numero1, numero2){
+function somaDeNumeros(numero1, numero2){
     return numero1 + numero2
 }
 
 function nomeIdade(nome, idade){
-    return `Meu nome e ${nome} e minha idade e ${idade}`
+    return ` Meu nome é ${nome} e minha idade é ${idade}`
 }
-// A ordem dos paramemtros e importemte
-imprimeTexto(nomeIdade("Eduardo", 17))
-imprimeTexto(nomeIdade(17, "Bia"))
+// A ordem dos parâmetros é importante!!
+imprimeTexto(nomeIdade("André", 17))
+imprimeTexto(nomeIdade(18, "Kauani"))
 
-imprimeTexto(multiplicaçao())
-//Voce pode definir um parametro inicial para sua funçao
-function multiplicaçao(numero4 = 2, numero5 =5){
+imprimeTexto(multiplicacao())
+// Você pode definir um parâmetro inicial para sua função!
+function multiplicacao(numero4 = 2,  numero5 = 5){
     return numero4 * numero5
 }
 
-imprimeTexto(multiplicaçao(soma(3,4), soma (5, 10)))
+imprimeTexto(multiplicacao(somaDeNumeros(3,4), somaDeNumeros(5,10)));
+
+// expressão de função
+
+const constanteSoma = function(nume1, nume2){return nume1 + nume2}
+
+console.log(constanteSoma(3,6))
+
+// Funções e var são "Listadas" no topo do arquivo
+
+// Arrow function
+
+const apresentaArrow = nome => `meu nome é ${nome}`;
+const somaEmFlecha = (nu1, nu2) => nu1 + nu2;
+
+console.log(apresentaArrow("André"))
+console.log(somaEmFlecha(2,4))
+
+const SomaNumeroPequenos = (num1, num2) => {
+    if(num1 > 10 || num2 > 10){
+        return "Essa função é apenas para números pequenos"
+    } else {
+        return num1 + num2 
+    }
+}
+
+console.log(SomaNumeroPequenos(2,3))
